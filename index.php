83ac9ac4b49236e850a5db60e1ec808bf48b1648
Zashiky <119845760+ZashikyOff@users.noreply.github.com>
2023-05-13 11:32:26 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+$title = "Accueil";
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Assets/core/css/main.css">
+    <title>L'atelier des Colibris - <?= $title ?></title>
+</head>
+<body class="index">
+    <img src="Assets/img/background.jpg" class="background">
+    <nav>
+        <img src="Assets/img/logo.png" class="logo">
+        <h1>L'atelier des Colibris</h1>
+        <ul>
+            <li><a href="">Articles</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+    <main>
+        <div class="left">
+
+        </div>
+        <div class="middle">
+
+        </div>
+        <div class="right">
+            
+        </div>
+    </main>
+</body>
+</html>
