91c6a498b339bdfaf5545c70dba5c2b2c64a5d58
Octave <o.lapchin@groupe-aen.info>
2023-05-16 16:57:46 +0200

    [ADD] - Added database connection

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    // Database
+    CONST DB_HOST = "mysql-octavecolibris.alwaysdata.net";
+    CONST DB_NAME = "octavecolibris_colibris";
+    CONST DB_USER = "313663_colibris";
+    CONST DB_PASS = "Colibris84!";
+
+    // SSH DATABASE ACCESS:
+    // https://ssh-octavecolibris.alwaysdata.net/
+?>
