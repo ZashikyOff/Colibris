28e1c7ce9a65ae57e95d911afa5414a30b32b8d2
Zashiky <119845760+ZashikyOff@users.noreply.github.com>
2023-05-17 09:48:03 +0200

    Update Front of Page

--- articles.php
+++ articles.php
@@ -5,13 +5,25 @@ $title = "Articles";
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Assets/core/css/main.css">
     <title>L'atelier des Colibris - <?= $title ?></title>
 </head>
+
 <body>
-    
+    <nav>
+        <img src="Assets/img/logo.png" class="logo">
+        <h1>L'atelier des Colibris</h1>
+        <ul>
+            <li><a href="articles.php">Articles</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
 </body>
+
 </html>
--- contact.php
+++ contact.php
@@ -5,13 +5,24 @@ $title = "Contact";
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Assets/core/css/main.css">
     <title>L'atelier des Colibris - <?= $title ?></title>
 </head>
+
 <body class="contact">
+    <nav>
+        <img src="Assets/img/logo.png" class="logo">
+        <h1>L'atelier des Colibris</h1>
+        <ul>
+            <li><a href="articles.php">Articles</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
     <h1>Pour nous Contacter</h1>
     <h4>Cette page sert a nous contacter par mail pour nous prevenir si vous voulez faire des dons a notre association</h4>
     <form action="" method="post" enctype="multipart/form-data">
@@ -21,4 +32,5 @@ $title = "Contact";
         <button type="submit">Envoyer</button>
     </form>
 </body>
+
 </html>
