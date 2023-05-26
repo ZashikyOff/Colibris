54ed17271fb313fc5e0a34316d2a724191b725a4
Zashiky <119845760+ZashikyOff@users.noreply.github.com>
2023-05-26 09:54:59 +0200

    [ADD] Front Card

--- Assets/core/config/config.php
+++ Assets/core/config/config.php
@@ -3,5 +3,5 @@
 // Connexion à la base de données
 $dsn = "mysql:host=localhost;port=3306;dbname=colibris;charset=utf8";
 $dbUser = "root";
-$dbPassword = "";
+$dbPassword = "root";
 $lienDB = new PDO($dsn, $dbUser, $dbPassword);
