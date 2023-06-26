864c49341643ee536769bccae35668cb6fce0e98
Zashiky <119845760+ZashikyOff@users.noreply.github.com>
2023-06-26 10:09:31 +0200

    [UPDATE] CSS Footer Index

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $title = "Accueil";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="Assets/core/css/main.css">
     <title>L'atelier des Colibris - <?= $title ?></title>
 </head>
@@ -59,7 +60,24 @@ $title = "Accueil";
         </div>
     </main>
     <footer>
-        Credit - &copy; Xenatil - 2023
+        <div class="top-footer">
+            <div class="info-footer">
+                <div class="footer-map">
+                    <i class="fa-solid fa-location-dot"></i>
+                    <p>Adress</p>
+                </div>
+                <div class="footer-phone">
+                    <i class="fa-solid fa-phone"></i>
+                    <p>Phone Number</p>
+                </div>
+                <div class="footer-email">
+                    <i class="fa-solid fa-envelope"></i>
+                    <p>Mail</p>
+                </div>
+            </div>
+            <img src="Assets/img/logo.png">
+        </div>
+        &copy;2023 Xenatil | All Rights Reserved
     </footer>
 </body>
 
